ea05d8112f95d80109b53f6667bbb0c359e12e9c
quakeX <iquakex@gmail.com>
2013-11-07 10:49:09 +0800

    day03 harib00j 实现 HLT,在 C 程序中调用汇编.

--- bootpack.c
+++ bootpack.c
@@ -1,8 +1,13 @@
+/* 告诉 C 编译器,有一个函数在别的文件里:函数的声明 */
+
+void io_hlt(void);
+
 void HariMain(void)
 {
 
 fin:
-/* 这里想写上 HLT,但 C 语言中不能用 HLT!*/
+   /* 执行 naskfunc.nas 里的_io_hlt*/
+	io_hlt();
 	goto fin;
 
 }
