008c02864854f752205e1138a5fb859bba705810
quakeX <iquakex@gmail.com>
2013-11-07 15:38:38 +0800

    day04-harib01e-指针操作

--- bootpack.c
+++ bootpack.c
@@ -1,14 +1,15 @@
 /* 告诉 C 编译器,有一个函数在别的文件里:函数的声明 */
 
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
 	int i;
+	char *p;
 	for (i = 0xa0000; i < 0xaffff; i++)
 	{
-		write_mem8(i, i&0x0f);
+		p = (char *) i;  // 代入地址
+		*p = i & 0x0f;
 	}
 
 	for(;;){
